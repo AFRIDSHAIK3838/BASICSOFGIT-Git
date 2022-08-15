a1ffecbdd98152e9e697e542828f66ebb5b5bc0e
AFRIDSHAIK3838 <111264778+AFRIDSHAIK3838@users.noreply.github.com>
2022-08-15 10:33:28 +0530

    Add files via upload

---
+++ ARRAYS.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ARRAYS {
+    
+    public static void main(String[] args) {
+        int sum=0;
+        int Avg;
+        int  A[]=new int[4];;
+        A[0]=10;
+        A[1]=20;
+        A[2]=30;
+        A[3]=40;
+        int B[]={50,60,70};
+        for(int i=0;i<4;i++){
+            System.out.println(A[i]);
+        }
+      for(int i=0;i<4;i++)
+      {
+        sum=sum+A[i];
+        System.out.println(sum);
+        Avg=sum/A.length;
+        System.out.println(Avg);
+      }
+      for(int i=0;i<B.length;i++)
+      {
+        System.out.println(B[i]);
+      }
+      int X;
+      Scanner sc=new Scanner(System.in);
+       X=sc.nextInt();
+     for(int i=0;i<4;i++)
+     {
+        if(A[i]==X){
+            System.out.println("Here is The Element"+i);
+        }
+     }
+
+    }
+}
